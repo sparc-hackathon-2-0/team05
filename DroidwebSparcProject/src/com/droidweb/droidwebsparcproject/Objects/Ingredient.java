6df9d9a1843848ef8299aff6446eb3789f182ac8
droidweb <triplemwebdesigns@gmail.com>
2012-08-25 11:04:04 -0400

    Added ingredient and recipe objects; began building out HTTPRequest object.
    FUSDORAH!!!

---
+++ DroidwebSparcProject/src/com/droidweb/droidwebsparcproject/Objects/Ingredient.java
@@ -0,0 +1,18 @@
+package com.droidweb.droidwebsparcproject.Objects;
+
+import java.util.ArrayList;
+
+
+public class Ingredient {
+	String ingredientName;
+	
+	public Ingredient(String ingrediString){
+		ingredientName = ingrediString;
+	}
+	
+	// get recipes for drink
+	public ArrayList<Recipes> getMyRecipes(){
+		// make query for recipes from drink
+		
+	}
+}
