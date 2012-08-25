2810922eec04add50d3b08fb4ce71ca9676fa77b
droidweb <triplemwebdesigns@gmail.com>
2012-08-25 11:19:11 -0400

    HTTPRequest Manager buildout --- make requests

--- DroidwebSparcProject/src/com/droidweb/droidwebsparcproject/DataManager/HTTPRequestManager.java
+++ DroidwebSparcProject/src/com/droidweb/droidwebsparcproject/DataManager/HTTPRequestManager.java
@@ -1,8 +1,18 @@
 package com.droidweb.droidwebsparcproject.DataManager;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.os.AsyncTask;
+import android.util.Log;
 
 public class HTTPRequestManager {
 
@@ -15,7 +25,29 @@ public class HTTPRequestManager {
 
 	private class HTTPRequestTask extends AsyncTask<URL, Integer, Long> {
 		protected Long doInBackground(URL... urls) {
-			
+
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httpost;
+			HttpResponse response;
+
+			for (int i = 0; i < urls.length; i++) {
+
+				httpost = new HttpPost(/* URL HERE */);
+
+				try {
+					response = httpclient.execute(httpost);
+					HttpEntity entity = response.getEntity();
+					InputStream stream = entity.getContent();
+					Log.d("response", ReadInputStream(stream));
+				} catch (ClientProtocolException e) {
+					// Handle not connecting to client
+					Log.d("ClientProtocolException Thrown", e.toString());
+				} catch (IOException e) {
+					// couldn't connect to host
+					// TODO: HANDLE NOT CONNECTING TO CLIENT
+					Log.d("IOException Thrown", e.toString());
+				}
+			}
 			return null;
 		}
 
@@ -25,8 +57,17 @@ public class HTTPRequestManager {
 
 		protected void onPostExecute(Long result) {
 			// we're done, parse the result
-			//parseResponse();
+			// parseResponse();
 		}
+	}
+
+	public static String ReadInputStream(InputStream in) throws IOException {
+		StringBuffer stream = new StringBuffer();
+		byte[] b = new byte[4096];
+		for (int n; (n = in.read(b)) != -1;) {
+			stream.append(new String(b, 0, n));
+		}
+		return stream.toString();
 	}
 
 }
