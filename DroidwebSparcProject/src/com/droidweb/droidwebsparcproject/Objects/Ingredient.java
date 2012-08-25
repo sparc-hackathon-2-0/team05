0e0e861ef3f26d9868a73a14b0a4f34d0b97e6c9
droidweb <triplemwebdesigns@gmail.com>
2012-08-25 11:59:43 -0400

    Updated Android Manifest.xml

--- DroidwebSparcProject/src/com/droidweb/droidwebsparcproject/Objects/Ingredient.java
+++ DroidwebSparcProject/src/com/droidweb/droidwebsparcproject/Objects/Ingredient.java
@@ -2,6 +2,9 @@ package com.droidweb.droidwebsparcproject.Objects;
 
 import java.util.ArrayList;
 
+import com.droidweb.droidwebsparcproject.DataManager.HTTPRequestManager;
+import com.droidweb.droidwebsparcproject.DataManager.RequestType;
+
 
 public class Ingredient {
 	String ingredientName;
@@ -12,8 +15,24 @@ public class Ingredient {
 	
 	// get recipes for drink
 	public ArrayList<Recipes> getMyRecipes(){
+		HTTPRequestManager requestManager = new HTTPRequestManager();
+		requestManager.makeRequest(RequestType.GetRecipesForIngredient, this.getIngredientName());
 		return null;
 		// make query for recipes from drink
 		
+	}
+
+	/**
+	 * @return the ingredientName
+	 */
+	public String getIngredientName() {
+		return ingredientName;
+	}
+
+	/**
+	 * @param ingredientName the ingredientName to set
+	 */
+	public void setIngredientName(String ingredientName) {
+		this.ingredientName = ingredientName;
 	}
 }
